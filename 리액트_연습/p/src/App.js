a1733872c0525e15a11599c0c27391b8d22df091
ohjaesik <ojaesig86@gmail.com>
2023-01-05 23:18:39 +0900

    벨로퍼트 1-5장

--- 리액트_연습/p/src/App.js
+++ 리액트_연습/p/src/App.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import Hello from './Hello'
+import Wrapper from './Wrapper'
 import './App.css'
 function App() {
   const name = 'react'
@@ -11,14 +12,14 @@ function App() {
     padding : '1rem'
   }
   return (
-    <>
+    <Wrapper>
     {/* 주석 사용하기 */}
-    
-      <Hello // 태그 내부에서 주석하용하기
+        <Hello name="react" color="red" // 태그 내부에서 주석하용하기
       />
+        <Hello color="pink"/>
       <div style={style}>{name}</div>
       <div className="gray-box"></div>
-    </>
+    </Wrapper>
   );
 }
 
--- 리액트_연습/p/src/Hello.js
+++ 리액트_연습/p/src/Hello.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function Hello() {
-  return <div>안녕하세요</div>
+function Hello(props /*비구조화 할당{color, name} */) {
+  return <div style={{color: props.color}}>안녕하세요 {props.name}</div>
+}
+
+Hello.defaultProps = {
+  name: '이름 없음'
 }
 
 export default Hello;
