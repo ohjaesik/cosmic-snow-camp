52f5ce91234f75d8eb36fd8f536aa6c5483d1880
ohjaesik <ojaesig86@gmail.com>
2023-01-07 13:21:29 +0900

    벨로퍼트 9.여러개의 input 상태 관리하기

--- 리액트_연습/p/src/InpustSample.js
+++ 리액트_연습/p/src/InpustSample.js
@@ -1,21 +1,34 @@
 import React, {useState} from 'react'
 
 function InputSample() {
-  const [text, setText] = useState('');
+  const [inputs, setInputs] = useState({
+    name:'',
+    nickname: '',
+  });
+  const {name, nickname} = inputs;
 
   const onChange = (e) => {
-    setText(e.target.value)
+    const {value, name} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
   }
 
   const onRest = () => {
-    setText("");
+    setInputs({
+      name:'',
+      nickname:'',
+    })
   }
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input placeholder="이름" name="name" onChange={onChange} value={name}/>
+      <input placeholder="닉네임" name="nickname" onChange={onChange} value={nickname}/>
       <button onClick={onRest}>초기화</button>
       <div>
-        <d>값 : {text}</d>
+        <d>값 :</d>
+        {name} ({nickname})
       </div>
     </div>
   )
