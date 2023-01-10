e6abfa5ed4df8ac5aba14003db271a9a842d0550
ohjaesik <96814750+ohjaesik@users.noreply.github.com>
2023-01-10 13:31:32 +0900

    userLIst

--- 리액트_연습/p/src/UserList.js
+++ 리액트_연습/p/src/UserList.js
@@ -1,34 +1,43 @@
-import React, {useEffect} from 'react'
+import React, { useContext } from 'react';
+import { UserDispatch } from './App';
 
-const User= React.memo(({user, onRemove, onToggle}) => {
-  useEffect(() => {
-    console.log('user 값이 설정됨');
-    console.log(user);
-    return () => {
-      console.log('user 가 바뀌기 전..');
-      console.log(user);
-    }
-  }, [user])
+const User = React.memo(function User({ user }) {
+  const dispatch = useContext(UserDispatch);
+
   return (
     <div>
-      <b style={{
-        cursor: 'pointer',
-        color: user.active ? 'green' : 'black'
-      }}
-      onClick= {() => onToggle(user.id)}>{user.username}</b> <span>({user.email})</span>
-      <button onClick={() => onRemove(user.id)}>삭제</button>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => {
+          dispatch({ type: 'TOGGLE_USER', id: user.id });
+        }}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button
+        onClick={() => {
+          dispatch({ type: 'REMOVE_USER', id: user.id });
+        }}
+      >
+        삭제
+      </button>
     </div>
-  )
-})
+  );
+});
 
-const UserList = ({users, onRemove, onToggle}) => {
-
+function UserList({ users }) {
   return (
     <div>
       {users.map(user => (
-        <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
+        <User user={user} key={user.id} />
       ))}
     </div>
-  )
+  );
 }
-export default React.memo(UserList)
+
+export default React.memo(UserList);
