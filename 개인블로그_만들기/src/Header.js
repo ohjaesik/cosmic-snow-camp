4b7ab2c4670c7ccb7a97aebc66de92c2f1214ed2
ohjaesik <ojaesig86@gmail.com>
2023-01-12 13:51:25 +0900

    css 적용

--- 개인블로그_만들기/src/Header.js
+++ 개인블로그_만들기/src/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './Header.scss'
 function HeaderDiv({title, number}) {
   return (
     <>
-      <div>
-        <p>{title}</p>
-        <p>{number}</p>
+      <div className="headerDiv">
+        <p>{title} <br></br> {number}</p>
+        
       </div>
     </>
   )
@@ -14,9 +15,10 @@ function Header({user}) {
     <>
       <h2>{user}</h2>
       <p>코스믹 회원</p>
+      <br></br>
       <HeaderDiv title='구독자' number='1270'/>
       <HeaderDiv title='관심작가' number='26'/>
-      <button>구독하기</button>
+      <button className="Button">구독하기</button>
     </>
   )
 }
--- 개인블로그_만들기/src/Magagine.js
+++ 개인블로그_만들기/src/Magagine.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Magagine.css'
 function Magagine() {
   const items = [
     {
