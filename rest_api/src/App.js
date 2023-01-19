d7d87bbc9a30a4a2d95c21ed67302a1a514dab69
ohjaesik <ojaesig86@gmail.com>
2023-01-19 23:58:03 +0900

    과제 2

--- rest_api/src/App.js
+++ rest_api/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import Header from './Header'
-import Main from './main'
+
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
-      <Main></Main>
     </React.Fragment>
   );
 }
--- rest_api/src/main.js
+++ rest_api/src/main.js
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
-function Main() {
+function Main(props) {
+  const [food, setFood] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getFood = async () => {
+      try{
+        setError(null);
+        setFood(null);
+        setLoading(true);
+      const response = await axios.get(
+        `https://openapi.foodsafetykorea.go.kr/api/5106e02c83604344bb93/I2790/json/1/100`
+    )
+    setFood(response.data.I2790.row)
+    
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+    }
+    getFood();
+  }, []);
+
+  if(loading) return <div>로딩중..</div>
+  if(error) return <div>에러가 발생했습니다.</div>
+  if(!food) return null;
+  if(props.foods) return <div>
+    
+  {food.filter((food) => 
+  ( <div className='food' > {food.DESC_KOR.toLowerCase().includes(props.foods)}</div>))}
+</div>
+  
   return (
     <React.Fragment>
       <p>아무 것도 존재 하지 않아</p>
